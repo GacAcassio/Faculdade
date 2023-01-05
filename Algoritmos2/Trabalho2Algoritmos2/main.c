7a703e0b4b146da73cfc9f62a355dfd1bb6f0f09
Gabriel Acassio <100379848+GacAcassio@users.noreply.github.com>
2023-01-04 23:01:29 -0400

    Update main.c

--- Algoritmos2/Trabalho2Algoritmos2/main.c
+++ Algoritmos2/Trabalho2Algoritmos2/main.c
@@ -259,6 +259,12 @@ void menuPrincipal()
 			      			system("cls");
 			      			system("COLOR F0");
 			      			listarFilme(-16);
+						gotoxy(62,35);
+						printf("Tecle[ENTER] para continuar"); 
+						while(menu!=13)
+						{
+							menu=getch();
+        					}
 			      			break;
 			      		case 2:
 			      			cadastroFilme();
